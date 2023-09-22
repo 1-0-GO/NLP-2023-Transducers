9e2cd70cf4608c8cabb86faa2a7334103a9e9eef
Francisco Martins <francisco.martins@tecnico.ulisboa.pt>
2023-09-22 23:16:07 +0100

    test: added all necessary tests Some are failing to compile others don't produce output

--- test.sh
+++ test.sh
@@ -19,4 +19,16 @@ test() {
     echo "\nThe end"
 }    
 
+# dates when we turned 18:
+# SEP/09/2019 JAN/01/2019
+
 test mmm2mm.fst JAN FEB DEC 
+test mix2numerical.fst SEP/20/2018 SEP/09/2019 JAN/01/2019
+test pt2en.fst SET/5/2018
+test en2pt.fst FEB/05/2081 SEP/09/2019 JAN/01/2019
+test day.fst 22
+test month.fst 9 09
+test year.fst 2025
+test datenum2text.fst 09/15/2055 SEP/09/2019 JAN/01/2019
+test mix2text.fst MAY/12/2088 MAI/12/2088 SEP/09/2019 JAN/01/2019
+test date2text.fst OCT/31/2025 OUT/31/2025 10/31/2025 SEP/09/2019 JAN/01/2019
